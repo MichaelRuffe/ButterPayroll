eedd7d22bbcacf17d399812dce24310fa182a596
mjelzon <mjelzon@gmail.com>
2014-10-30 15:04:37 -0400

    added add/modify form

--- ButterPayroll/AddModify.cs
+++ ButterPayroll/AddModify.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ButterPayroll
 {
-    public partial class Form1 : Form
+    public partial class AddModify : Form
     {
-        public Form1()
+        public AddModify()
         {
             InitializeComponent();
         }
--- ButterPayroll/Program.cs
+++ ButterPayroll/Program.cs
@@ -16,7 +16,7 @@ namespace ButterPayroll
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
